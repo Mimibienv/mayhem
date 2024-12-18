0a4292c8e2e3c138dce4787143adbe4207dd02c2
Mimibienv <82171237+Mimibienv@users.noreply.github.com>
2024-12-18 12:13:53 +0100

    even better
    sure it is

--- PlatesHandler.lua
+++ PlatesHandler.lua
@@ -10,12 +10,12 @@ local Settings = {
 	Space = 10;
 	DefaultPlateSize = 16;
 	Grid = 7;
-	NumberOfPlates = 49;
+	--NumberOfPlates = 49;
 }
 
 
 --// Functions
-local function RNGv2(min:number, max:number, n:number)
+local function RNGv2(min:number, max:number, n:number):()->{}
 	local outcomes,results = {},{}
 	for i=min,max do table.insert(outcomes, i) end
 	for i = 1, n do
@@ -32,10 +32,18 @@ local function GeneratePlates()
 	local Origin = Settings.Origin
 	local DefaultPlateSize = Settings.DefaultPlateSize
 	local Space = Settings.Space
+	local CurrentPlayers = Players:GetPlayers()
 	
-	local NumberOfPlates = RNGv2(1, Grid^2, #Players:GetPlayers())	
-	for Index, Plate in NumberOfPlates do
-		print(Plate)
+	local NumberOfPlates = RNGv2(1, Grid^2, #CurrentPlayers)	
+	for Index, PlateId in NumberOfPlates do
+		print(PlateId)
+		local CurrentPlayer = CurrentPlayers[Index] or nil
+		if CurrentPlayer==nil
+			or CurrentPlayer.Character==nil
+			or not CurrentPlayer.Character:FindFirstChildOfClass("Humanoid")
+			or CurrentPlayer.Character:FindFirstChildOfClass("Humanoid").Health<=0
+			or not CurrentPlayer.Character:FindFirstChild("Head")
+		then print("Couldn't load plate") continue end
 		--[[ How it works
 		1) Plate-1 for an origin of 0 instead of 1
 		2) Calculate its modulo grid for the x cuz every "grid" studs is a new line. Therefore, for Y it's gonna be the quotient (without the x rest)
@@ -43,47 +51,132 @@ local function GeneratePlates()
 		4) Since we want it to be centered, we remove 1 to the grid and again, multiply it by the space between each plate center.
 		   we remove 1 so that, if grid is 7, it's 6/2 so theres 3 on the right and 3 on the left. And if it's 8, it's 3.5 on right and 3.5 on left.
 		]]
-		local x = ((Plate-1)%Grid)*(DefaultPlateSize+Space)  - ((Grid-1)/2)*(DefaultPlateSize+Space)
-		local y = ((Plate-1)//Grid)*(DefaultPlateSize+Space) - ((Grid-1)/2)*(DefaultPlateSize+Space)
+		local x = ((PlateId-1)%Grid)*(DefaultPlateSize+Space)  - ((Grid-1)/2)*(DefaultPlateSize+Space)
+		local y = ((PlateId-1)//Grid)*(DefaultPlateSize+Space) - ((Grid-1)/2)*(DefaultPlateSize+Space)
 
 
 		local PlateInstance = Instance.new("Part")
 		PlateInstance.Anchored=true
-		PlateInstance.Name=tostring(Plate)
+		PlateInstance.Name=tostring(PlateId)
 		PlateInstance.EnableFluidForces=false
 		PlateInstance.Size=Vector3.new(DefaultPlateSize,1,DefaultPlateSize)
-		PlateInstance.Position=Origin + Vector3.new(x,0,y)
 		PlateInstance.Parent=PlatesDir
+		PlateInstance.Position=Origin + Vector3.new(x,0,y)
 		
-		Plates[Plate]=PlateInstance
+		Plates[Index]={
+			["Plate"] = PlateInstance;
+			["PlateId"] = PlateId;
+			["OwnerId"] = CurrentPlayer.UserId
+		}
+	end
+	
+	return true -- success
+end
+
+local function GetPlateFromOwnerId(OwnerId:string)
+	for i, v in Plates do
+		if v["OwnerId"] == OwnerId then
+			return v
+		end
+	end
+end
+local function GetPlateFromPlateId(OwnerId:string)
+	for i, v in Plates do
+		if v["PlateId"] == OwnerId then
+			return v
+		end
 	end
 end
 
-
 --// Module connections
+
+-- Set a default plate setting
 PlatesHandler.SetSetting = function(setting, value)
 	Settings[setting] = value
 end
 
+-- Get a default plate setting
 PlatesHandler.GetSetting = function(setting)
 	return Settings[setting]
 end
 
-PlatesHandler.GeneratePlates = function()
-	GeneratePlates()
+-- Generate the plates
+PlatesHandler.GeneratePlates = GeneratePlates
+
+-- Get plate(s)
+PlatesHandler.GetPlates = function(Index)
+	return Plates
+end
+PlatesHandler.GetPlateFromOwnerId = function(x)
+	if type(x)=="number" and Players:GetPlayerByUserId(x) then
+		return GetPlateFromOwnerId(x)
+	elseif type(x)=="table" then
+		local PlatesResult = {}
+		for i, v in x do
+			if typeof(v)=="number" and Players:GetPlayerByUserId(v) then
+				table.insert(PlatesResult,GetPlateFromOwnerId(v))
+			end
+		end
+		return PlatesResult
+	end
+end
+PlatesHandler.GetPlateFromIndex = function(x)
+	if type(x)=="number" and table.find(Plates,x) then
+		return Plates[x]
+	elseif type(x)=="table" then
+		local PlatesResult = {}
+		for i, v in x do
+			if typeof(v)=="number" and table.find(Plates,x) then
+				table.insert(PlatesResult,GetPlateFromOwnerId(v))
+			end
+		end
+		return PlatesResult
+	end
+end
+PlatesHandler.GetPlateFromPlateId = function(x)
+	if type(x)=="number" and Players:GetPlayerByUserId(x) then
+		return GetPlateFromPlateId(x)
+	elseif type(x)=="table" then
+		local PlatesResult = {}
+		for i, v in x do
+			if typeof(v)=="number" and Players:GetPlayerByUserId(v) then
+				table.insert(PlatesResult,GetPlateFromPlateId(v))
+			end
+		end
+		return PlatesResult
+	end
 end
 
-PlatesHandler.RemovePlate = function()
+
+PlatesHandler.ModifyPlate = function(PlateId,Function)
+	
+end
+
+PlatesHandler.RemovePlate = function(PlateId)
+	if type(PlateId)=="number" then
+		local Plate = GetPlateFromPlateId(PlateId)
+		if Plate then
+			Plate.Plate:Destroy()
+			table.remove(Plates,Plate)
+		end
+	elseif type(PlateId)=="table" then
+		for i, v in PlateId do
+			local Plate = GetPlateFromPlateId(v)
+			if Plate then
+				Plate.Plate:Destroy()
+				table.remove(Plates,Plate)
+			end
+		end
+	end
 end
 
 PlatesHandler.ClearPlates = function()
-end
-
-PlatesHandler.GetPlates = function()
-end
-
-PlatesHandler.ModifyPlate = function()
+	for i, v in Plates do
+		if v.Plate~=nil then v.Plate:Destroy() end
+	end
+	Plates = {}
 end
 
 --// Return
+export type PlateObject = {Plate:Part, PlateId:number, OwnerId:number}
 return PlatesHandler
