945422208a9423921852c04357f25171d4d0e159
Mimibienv <82171237+Mimibienv@users.noreply.github.com>
2024-12-21 20:22:01 +0100

    Update PlatesEvents.lua

--- PlatesEvents.lua
+++ PlatesEvents.lua
@@ -2,17 +2,19 @@
 local PlatesEvent = {
 	Events = {};
 	Functions = {};
+	Variables = {};
 }
 local Events, Functions = table.unpack(PlatesEvent)
 export type PlateObject<table> = {Plate:Part, PlateId:number, OwnerId:number}
 export type EventObject<table> = {
 	Data : {
-		name : string|nil,
-		description : string|nil,
-		min : number|nil;
-		max : number|nil;
-		yield : boolean|nil;
-		visualize : boolean|nil;
+		name : string|nil,          -- Name of the event
+		description : string|nil,   -- Description of the event
+		playerEvent : boolean|nil,  -- If this event happens to specific players/plates. If false, then it's global.
+		min : number|nil;           -- Minimum affected players / Can be expressed in percentage [0;1].
+		max : number|nil;           -- Maximum affected players / Can be expressed in percentage [0;1].
+		yield : boolean|nil;        -- Tells if the main script should wait for the event to finish before continuing.
+		visualize : boolean|nil;    -- Highlight the affected plates if true.
 	},
 	Event : (Plate<PlateObject>) -> (ExtraData<table>)
 }
@@ -27,16 +29,24 @@ PlatesEvent.Events["RandomPlateColor"] = {
 		name = "name fr";
 		description = "desc"; 
 		subtitle = "'s plate will be colored";
-		min = 1;
-		max = 49;
+		playerEvent = true;
+		min = 0.25;
+		max = 0.5;
 		yield = true;
 		visualize = true;
 	};
-	Event = function(Plate:PlateObject, ExtraData)
+	Event = function(Plate:PlateObject)
 		Plate.Plate.Color = Color3.fromRGB(math.random(0,255), math.random(0,255), math.random(0,255))
 	end;
 }
 
+--// Variables 
+
+PlatesEvent.Variables["NumberOfEvents"] = 0
+for i in PlatesEvent.Events do PlatesEvent.Variables["NumberOfEvents"]+=1 end
+
+PlatesEvent.Variables["EveryEventsName"] = {}
+for i in PlatesEvent.Events do table.insert(PlatesEvent.Variables["EveryEventsName"], i) end
 
 --// Module connections
 
