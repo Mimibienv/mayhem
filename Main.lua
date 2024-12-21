8d43072db2a050f919b8da381404ca1742dc279d
Mimibienv <82171237+Mimibienv@users.noreply.github.com>
2024-12-21 20:21:45 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -14,7 +14,22 @@ local CurrentPlayers = {}
 local MatchInProgess = false
 local EventInProgress = false
 
+local Events = PlatesEvents.Events
+local EveryEventsName = PlatesEvents.Variables.EveryEventsName
+local NumberOfEvents = PlatesEvents.Variables.NumberOfEvents
+
 --// Functions
+local function RNGv2(min:number, max:number, n:number):()->{}
+	local outcomes,results = {},{}
+	for i=min,max do table.insert(outcomes, i) end
+	for i = 1, n do
+		local index = math.random(1, #outcomes)
+		table.insert(results, outcomes[index])
+		table.remove(outcomes, index)
+	end
+	return results
+end
+
 local function TeleportAllPlayers()
 	for i, v in Players:GetPlayers() do
 		local PlayerPlate = PlatesHandler.GetPlateFromOwnerId(v.UserId)
@@ -25,7 +40,7 @@ local function TeleportAllPlayers()
 	end
 end
 
-local function CheckNumberOfPlayers()
+local function UpdatePlayerCounts()
 	CurrentPlayers = Players:GetPlayers()
 	CurrentNumberOfPlayers = #CurrentPlayers
 	if CurrentNumberOfPlayers >= RequiredPlayersNumber then
@@ -44,6 +59,9 @@ local function CheckNumberOfPlayers()
 	end
 end
 
+local function PickRandomEvent()
+	return Events[EveryEventsName[math.random(1,NumberOfEvents)]]
+end
 
 --// Connect
 
@@ -51,19 +69,61 @@ end
 while not MatchInProgess and task.wait(1) do
 	MatchInProgess = true
 	
-	local PlayerThreshold = CheckNumberOfPlayers()
-	print(CurrentPlayers)
+	local PlayerThreshold = UpdatePlayerCounts()
+	print("Current players:", CurrentPlayers)
 	
 	if PlayerThreshold then
 		local Reponse = PlatesHandler.GeneratePlates()
 		repeat task.wait() until Reponse==true
 		TeleportAllPlayers()
+		wait(3)
 		
-		wait(3) -- Delay after all players got teleported
+		local RngEvent = PickRandomEvent()
+		local Data = RngEvent.Data
+		print("RngEvent:", RngEvent)
 		
-		-- for each event
-		PlatesEvents.Events.RandomPlateColor.Event(PlatesHandler.GetPlateFromOwnerId(369742023))
-		print("\n")
+		if Data.playerEvent~=nil and Data.playerEvent then
+			local min, max												--------------------------------------
+			if Data.min then																				--
+				if Data.min<1 then																			--
+					min = math.round(CurrentNumberOfPlayers*Data.min)										--
+				else																						--
+					min = Data.min																			--
+				end																							--
+				if min<1 then min=1 end																		--
+			else																							--
+				min=1																						--
+			end																								--
+			if Data.max then																				--
+				if Data.max<1 then																			-- min / max
+					max = (Data.max>=Data.min) and math.round(CurrentNumberOfPlayers*Data.max) or min		-- Algorithm
+				else																						--
+					max = (Data.max>=Data.min) and Data.max or min											--
+				end																							--
+				if max>CurrentNumberOfPlayers then max=CurrentNumberOfPlayers end							--
+			else																							--
+				max=CurrentNumberOfPlayers																	--
+			end																								--
+			print("min, max =", min, max)																	--
+																											--
+			local AffectedPlates = {}																		--
+			local ChosenPlayers = RNGv2(1, CurrentNumberOfPlayers, math.random(min, max))					--
+			print("Chosen Players:", ChosenPlayers)											------------------
+			
+			for i, v in ChosenPlayers do
+				local PlayerPlate = PlatesHandler.GetPlateFromIndex(v)  --GetPlateFromOwnerId(CurrentPlayers[v].UserId)
+				if PlayerPlate~=nil then
+					table.insert(AffectedPlates, PlayerPlate)
+				end
+			end
+			for i, v in AffectedPlates do
+				RngEvent.Event(v)
+			end
+			print("Affected plates:", AffectedPlates)
+		end
+		
+		--RngEvent.Event(PlatesHandler.GetPlateFromOwnerId(369742023))
+		print("\n========================\n")
 	end
 	
 	MatchInProgess = false
