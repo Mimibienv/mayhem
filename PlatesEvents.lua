7ae9796675ff653f9d0e74fade1ada17cfae3b05
Mimibienv <82171237+Mimibienv@users.noreply.github.com>
2024-12-27 16:49:31 +0100

    Update PlatesEvents.lua

--- PlatesEvents.lua
+++ PlatesEvents.lua
@@ -1,10 +1,13 @@
 --// Variables
+local ServerStorage = game:GetService("ServerStorage")
+local EventsDirectory = ServerStorage:WaitForChild("Events")
+
 local PlatesEvent = {
 	Events = {};
 	Functions = {};
 	Variables = {};
 }
-local Events, Functions = table.unpack(PlatesEvent)
+
 export type PlateObject<table> = {Plate:Part, PlateId:number, OwnerId:number}
 export type EventObject<table> = {
 	Data : {
@@ -13,7 +16,7 @@ export type EventObject<table> = {
 		playerEvent : boolean|nil,  -- If this event happens to specific players/plates. If false, then it's global.
 		min : number|nil;           -- Minimum affected players / Can be expressed in percentage [0;1].
 		max : number|nil;           -- Maximum affected players / Can be expressed in percentage [0;1].
-		yield : boolean|nil;        -- Tells if the main script should wait for the event to finish before continuing.
+		cooldown : number|nil;      -- Tells if the main script should wait for the event to finish before continuing.
 		visualize : boolean|nil;    -- Highlight the affected plates if true.
 	},
 	Event : (Plate<PlateObject>) -> (ExtraData<table>)
@@ -24,21 +27,9 @@ export type EventObject<table> = {
 
 --// Events
 
-PlatesEvent.Events["RandomPlateColor"] = {
-	Data = {
-		name = "name fr";
-		description = "desc"; 
-		subtitle = "'s plate will be colored";
-		playerEvent = true;
-		min = 0.25;
-		max = 0.5;
-		yield = true;
-		visualize = true;
-	};
-	Event = function(Plate:PlateObject)
-		Plate.Plate.Color = Color3.fromRGB(math.random(0,255), math.random(0,255), math.random(0,255))
-	end;
-}
+for i, EventModule:ModuleScript in EventsDirectory:GetChildren() do
+	PlatesEvent.Events[EventModule.Name]=require(EventModule)
+end
 
 --// Variables 
 
